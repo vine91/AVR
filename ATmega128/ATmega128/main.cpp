cb27ca79a10fcf97a68bc9188846db5d166435ff
CLJ <vine9151@gmail.com>
2021-05-13 23:10:24 +0900

    editFiles

---
+++ ATmega128/ATmega128/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * ATmega128.cpp
+ *
+ * Created: 2021-05-13 오전 10:16:12
+ * Author : user
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
---
+++ ATmega128A/ATmega128A/0513.cpp
@@ -0,0 +1,44 @@
+﻿/*
+ *	  NOTE - 0513.cpp
+ *
+ * Created - 2021.05.13
+ *  Author - 2033112012 CLJ
+ */
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+#pragma GCC diagnostic ignored "-Wwrite-strings"
+
+#include "MCU.HPP"
+
+
+int main (void)
+{
+	
+    Setting::initPort(F, OUT);
+    LCD LCD1;
+	LCD1.beginLCD(F);
+	LCD1.initLCD();
+    
+    while (1)
+    {
+		LCD1.setInstruction(0x80);
+		LCD1.setData(0x31);
+		LCD1.setData(0x32);
+    }
+	
+	/*
+	Setting::initPort(A, OUT);
+	
+	while (1)
+	{
+		BL(PORTA, 6);
+		_delay_ms(500);
+		
+		
+	}
+	*/
+}
+
+
