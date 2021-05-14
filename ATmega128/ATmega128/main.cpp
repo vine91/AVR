2c01f006b7f10e9d89cac972dbe5992c26b9245b
CLJ <vine9151@gmail.com>
2021-05-15 02:12:01 +0900

    210515_2_11 addSW

--- ATmega128/ATmega128/main.cpp
+++ ATmega128/ATmega128/main.cpp
@@ -6,13 +6,59 @@
  */
 
 #include <avr/io.h>
+#include <util/delay.h>
 
+#include "MCU.HPP"
+
+
+
+
+/*========================================//
+-------------   MAIN START   ---------------
+//========================================*/
 
 int main (void)
 {
-    /* Replace with your application code */
+	
+    Setting::beginPort(A, OUT);
+	Setting::beginPort(C, IN);
+	Setting::beginPort(F, OUT);
+	
+	SW portC;
+	LCD portF;
+	
+	portC.beginSW(C, ON);
+	portF.beginLCD(F, HIGH);
+	portF.initLCD();
+	
+	PORTA = 0x01;
+	
+	
     while (true) 
     {
+		portC.initSW();
+		
+		if (portC.result != SW_OFF)
+		{
+			switch (portC.result)
+			{
+				case SW_0:
+					PORTA = PORTA << 1;
+					break;
+					
+				case SW_1:
+					PORTA = PORTA >> 1;
+			}
+		}
+		
+		portF.changeLine(1, 3);
+		portF.writeString("Testing System");
+		portF.changeLine(2, 11);
+		portF.writeString("by CLJ");
     }
+	
 }
 
+/*========================================//
+--------------   MAIN END   ----------------
+//========================================*/
