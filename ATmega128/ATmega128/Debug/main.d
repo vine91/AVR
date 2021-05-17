b2c003bc05f029f99cf925a934f0bf91f5fc50b3
CLJ <vine9151@gmail.com>
2021-05-17 17:13:22 +0900

    210517_17_12 AddInterrupt

--- ATmega128/ATmega128/Debug/main.d
+++ ATmega128/ATmega128/Debug/main.d
@@ -10,6 +10,7 @@ main.d main.o: .././main.cpp \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
@@ -36,6 +37,8 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
 
--- ATmega128/ATmega128/main.cpp
+++ ATmega128/ATmega128/main.cpp
@@ -6,9 +6,28 @@
  */
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
 
 #include "MCU.HPP"
+
+
+
+
+SW sw;
+LCD lcd;
+
+ISR(TIMER2_OVF_vect);
+ISR(TIMER1_COMPA_vect);
+
+int Title = ON;
+int Start = OFF;
+int Pause = ON;
+int Set = OFF;
+int Setted = OFF;
+
+int i, j;
+int k, l;
 
 
 
@@ -20,41 +39,34 @@
 int main (void)
 {
 	
-    Setting::beginPort(A, OUT);
-	Setting::beginPort(C, IN);
-	Setting::beginPort(F, OUT);
+    Setting::beginPort(C, IN);
+	Setting::beginPort(E, OUT);
 	
-	SW portC;
-	LCD portF;
+	Setting::beginTimer(2, OVF);
+	Setting::beginTimer(1, COMP);
 	
-	portC.beginSW(C, ON);
-	portF.beginLCD(F, HIGH);
-	portF.initLCD();
+	sw.beginSW(C, ON);
+	lcd.beginLCD(E, HIGH);
+	lcd.initLCD();
 	
-	PORTA = 0x01;
+	sei();
 	
 	
     while (true) 
     {
-		portC.initSW();
+		_delay_ms(2);
 		
-		if (portC.result != SW_OFF)
+		if (Title == ON)
 		{
-			switch (portC.result)
-			{
-				case SW_0:
-					PORTA = PORTA << 1;
-					break;
-					
-				case SW_1:
-					PORTA = PORTA >> 1;
-			}
+			lcd.changeLine(1, 2);
+			lcd.printLCD("Counter System");
+			lcd.changeLine(2, 11);
+			lcd.printLCD("by CLJ");
+			
+			_delay_ms(300);
 		}
 		
-		portF.changeLine(1, 3);
-		portF.writeString("Testing System");
-		portF.changeLine(2, 11);
-		portF.writeString("by CLJ");
+		_delay_ms(2);
     }
 	
 }
@@ -62,3 +74,254 @@ int main (void)
 /*========================================//
 --------------   MAIN END   ----------------
 //========================================*/
+
+
+
+
+ISR(TIMER2_OVF_vect)
+{
+	sw.initSW();
+	
+	if (sw.result != SW_OFF)
+	{
+		switch (sw.result)
+		{
+			case SW_0:				// Start and Restart when Time's set
+				Title = OFF;
+				
+				if (Set == ON && Setted == OFF)
+				{
+					lcd.clearLine(ALL);
+					
+					lcd.changeLine(1, 1);
+					lcd.printLCD("Counting");
+					
+					Start = ON;
+					Pause = OFF;
+				}
+				
+				else if (Set == OFF && Setted == OFF)
+				{
+					lcd.clearLine(ALL);
+					
+					lcd.changeLine(1, 1);
+					lcd.printLCD("Please Push SW 3");
+					lcd.changeLine(2, 2);
+					lcd.printLCD("to Set the Time");
+					
+					_delay_ms(4000);
+					
+					lcd.clearLine(ALL);
+					
+					Title = ON;
+				}
+				
+				break;
+				
+			case SW_1:				// Pause when Time's started
+				Pause = ON;
+				break;
+				
+			case SW_2:				// Go to Setting when Time's not set
+				Title = OFF;
+				
+				if (Set == OFF && Setted == OFF)
+				{
+					Setted = ON;
+					
+					lcd.clearLine(ALL);
+					
+					lcd.changeLine(1, 1);
+					lcd.printLCD("Set Time");
+					
+					lcd.changeLine(2, 12);
+					lcd.setData(EncodeLCD(i));
+					lcd.setData(EncodeLCD(j));
+					lcd.printLCD(":");
+					lcd.setData(EncodeLCD(k));
+					lcd.setData(EncodeLCD(l));
+				}
+				
+				break;
+			
+			case SW_3:				// Set the Time when Time's not set and it's in Setting
+				if (Set == OFF && Setted == ON)
+				{
+					Set = ON;
+					Setted = OFF;
+					
+					lcd.clearLine(1);
+					
+					lcd.changeLine(1, 1);
+					lcd.printLCD("Set Complete");
+					
+					_delay_ms(2000);
+					
+					lcd.clearLine(ALL);
+					
+					Title = ON;
+				}
+				
+				break;
+			
+			case SW_4:				// Control the Time when it's in Setting
+				if (Set == OFF && Setted == ON)
+				{
+					if (i != 5)
+					{
+						i++;
+					}
+					
+					else
+					{
+						i = 0;
+					}
+					
+					lcd.changeLine(2, 12);
+					lcd.setData(EncodeLCD(i));
+					lcd.setData(EncodeLCD(j));
+					lcd.printLCD(":");
+					lcd.setData(EncodeLCD(k));
+					lcd.setData(EncodeLCD(l));
+				}
+				
+				break;
+				
+			case SW_5:
+				if (Set == OFF && Setted == ON)
+				{
+					if (j != 9)
+					{
+						j++;
+					}
+					
+					else
+					{
+						j = 0;
+					}
+					
+					lcd.changeLine(2, 12);
+					lcd.setData(EncodeLCD(i));
+					lcd.setData(EncodeLCD(j));
+					lcd.printLCD(":");
+					lcd.setData(EncodeLCD(k));
+					lcd.setData(EncodeLCD(l));
+				}
+				
+				break;
+			
+			case SW_6:
+				if (Set == OFF && Setted == ON)
+				{
+					if (k != 5)
+					{
+						k++;
+					}
+					
+					else
+					{
+						k = 0;
+					}
+					
+					lcd.changeLine(2, 12);
+					lcd.setData(EncodeLCD(i));
+					lcd.setData(EncodeLCD(j));
+					lcd.printLCD(":");
+					lcd.setData(EncodeLCD(k));
+					lcd.setData(EncodeLCD(l));
+				}
+				
+				break;
+			
+			case SW_7:
+				if (Set == OFF && Setted == ON)
+				{
+					if (l != 9)
+					{
+						l++;
+					}
+					
+					else
+					{
+						l = 0;
+					}
+					
+					lcd.changeLine(2, 12);
+					lcd.setData(EncodeLCD(i));
+					lcd.setData(EncodeLCD(j));
+					lcd.printLCD(":");
+					lcd.setData(EncodeLCD(k));
+					lcd.setData(EncodeLCD(l));
+				}
+				
+				break;
+			
+			default:
+				break;
+		}
+	}
+}
+
+
+ISR(TIMER1_COMPA_vect)
+{
+	if (Start == ON)
+	{
+		if (Pause == OFF)
+		{
+			
+			lcd.changeLine(2, 12);
+			
+			lcd.setData(EncodeLCD(i));
+			lcd.setData(EncodeLCD(j));
+			lcd.printLCD(":");
+			lcd.setData(EncodeLCD(k));
+			lcd.setData(EncodeLCD(l));
+			
+			l--;
+			
+			if (l < 0)
+			{
+				l = 9;
+				k--;
+				
+				if (k < 0)
+				{
+					k = 5;
+					j--;
+					
+					if (j < 0)
+					{
+						j = 9;
+						i--;
+						
+						if (i < 0)
+						{
+							i = 0;
+							j = 0;
+							k = 0;
+							l = 0;
+							
+							lcd.clearLine(1);
+							
+							lcd.changeLine(1, 1);
+							lcd.printLCD("Done");
+							
+							Start = OFF;
+							Set = OFF;
+						}
+					}
+				}
+			}
+			
+		}
+		
+		else
+		{
+			lcd.clearLine(1);
+			
+			lcd.changeLine(1, 1);
+			lcd.printLCD("Pause");
+		}
+	}
+}
