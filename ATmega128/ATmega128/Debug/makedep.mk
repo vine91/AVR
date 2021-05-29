6945e0aa0ddb2c433589e6a0adf0b0bb44a4a0f0
CLJ <vine9151@gmail.com>
2021-05-30 02:43:17 +0900

    210530_2_43 corrected

--- ATmega128/ATmega128/Debug/makedep.mk
+++ ATmega128/ATmega128/Debug/makedep.mk
@@ -2,11 +2,11 @@
 # Automatically-generated file. Do not edit or delete the file
 ################################################################################
 
-counter_system.cpp
+line_tracer.cpp
 
 Modules\LCD.cpp
 
-Modules\mcu.cpp
+Modules\MCU.cpp
 
 Modules\motor.cpp
 
