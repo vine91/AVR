53f591212c48da187386e6630bf0867b77446100
CLJ <vine9151@gmail.com>
2021-05-31 19:55:33 +0900

    210531_19_55 updatedTracer

--- ATmega128/ATmega128/Line_Tracer.cpp
+++ ATmega128/ATmega128/Line_Tracer.cpp
@@ -64,106 +64,6 @@ int main(void)
     while (true) 
     {
 		
-		newdata = PINE;
-		detection = olddata & (~ newdata);
-		
-		if (detection != 0x00)
-		{
-			switch (detection)
-			{
-				case 0x01:
-					sensorOff();
-					left = ON;
-					break;
-					
-				case 0x05:
-					sensorOff();
-					leftMiddle = ON;
-					break;
-					
-				case 0x04:
-					sensorOff();
-					middle = ON;
-					break;
-					
-				case 0x14:
-					sensorOff();
-					rightMiddle = ON;
-					break;
-					
-				case 0x10:
-					sensorOff();
-					right = ON;
-					break;
-					
-				case 0x15:
-					sensorOff();
-					all = ON;
-				
-				default:
-					break;
-			}
-		}
-
-		olddata = newdata;
-		
-		if (left == ON)
-		{
-			PORTD = 0x01;
-			_delay_us(7);
-			PORTD = 0x00;
-			
-			isOn = false;
-		}
-		
-		else if (leftMiddle == ON)
-		{
-			PORTD = 0x08;
-			_delay_us(8);
-			PORTD = 0x00;
-			
-			isOn = false;
-		}
-		
-		else if (middle == ON)
-		{
-			PORTD = 0x01;
-			PORTC = 0x01;
-			_delay_us(7);
-
-			PORTD = 0x00;
-			PORTC = 0x00;
-			
-			isOn = false;
-		}
-		
-		else if (rightMiddle == ON)
-		{
-			PORTC = 0x01;
-			_delay_us(8);
-			PORTC = 0x00;
-			
-			isOn = false;
-		}
-		
-		else if (right == ON)
-		{
-			PORTC = 0x01;
-			_delay_us(7);
-			PORTC = 0x00;
-			
-			isOn = false;
-		}
-		
-		else if (all == ON)
-		{
-			PORTC = 0x08;
-			_delay_us(7);
-			PORTC = 0x00;
-			
-			isOn = false;
-		}
-		
     }
 	
 	return 0;
@@ -185,24 +85,21 @@ void sensorOff (void)
 }
 
 
-/*
 ISR(TIMER0_OVF_vect)
 {
-	cli();
 	
-	sw.init();
+	newdata = PINB;
+	detection = olddata & (~ newdata);
 	
-	if (sw.result != SW_OFF)
+	if (detection != 0x00)
 	{
-		_delay_ms(10);
-		
-		switch (sw.result)
+		switch (detection)
 		{
-			case SW_0:
+			case 0x01:
 				startSwitch = OFF;
 				break;
 			
-			case SW_1:
+			case 0x02:
 				startSwitch = ON;
 				break;
 			
@@ -213,57 +110,108 @@ ISR(TIMER0_OVF_vect)
 	
 	if (startSwitch == ON)
 	{
-		isON = ON;
+		leftmotor.start(CW);
+		rightMotor.start(CCW);
+
+		leftMotor.stop();
+		rightMotor.stop();
 	}
 	
 	else
 	{
-		PORTD = 0x00;
-		
-		isON = OFF;
+		leftMotor.stop();
+		rightMotor.stop();
 	}
 	
-	sei();
 }
 
 
 ISR(TIMER2_COMP_vect)
 {
 	
-	sensor.init();
+	newdata = PINE;
+	detection = olddata & (~ newdata);
 	
-	if (isON == true)
+	if (detection != 0x00)
 	{
-		if (sensor.result != SW_OFF)
+		switch (detection)
 		{
-			switch (sensor.result)
-			{
-				case SW_0:
-					PORTD = 0x08;
-					_delay_us(1);
-					PORTD = 0x00;
-					break;
+			case 0x01:
+				sensorOff();
+				left = ON;
+				break;
 				
-				case SW_1:
-					PORTD = 0x09;
-					_delay_us(1);
-					PORTD = 0x00;
-					break;
+			case 0x05:
+				sensorOff();
+				leftMiddle = ON;
+				break;
 				
-				case SW_2:
-					PORTD = 0x01;
-					_delay_us(1);
-					PORTD = 0x00;
-					break;
+			case 0x04:
+				sensorOff();
+				middle = ON;
+				break;
 				
-				default:
-					PORTD = 0x09;
-					_delay_us(1);
-					PORTD = 0x00;
-					break;
-			}
+			case 0x14:
+				sensorOff();
+				rightMiddle = ON;
+				break;
+				
+			case 0x10:
+				sensorOff();
+				right = ON;
+				break;
+				
+			case 0x15:
+				sensorOff();
+				all = ON;
+			
+			default:
+				break;
 		}
 	}
+
+	olddata = newdata;
 	
+	if (left == ON)
+	{
+		leftMotor.setSpeed(6);
+		rightMotor.setSpeed(7);
+	}
+	
+	else if (leftMiddle == ON)
+	{
+		leftMotor.setSpeed(7);
+		rightMotor.setSpeed(10);
+	}
+	
+	else if (middle == ON)
+	{
+		leftMotor.setSpeed(10);
+		rightMotor.setSpeed(10);
+	}
+	
+	else if (rightMiddle == ON)
+	{
+		leftMotor.setSpeed(10);
+		rightMotor.setSpeed(7);
+	}
+	
+	else if (right == ON)
+	{
+		leftMotor.setSpeed(7);
+		rightMotor.setSpeed(6);
+	}
+	
+	else if (all == ON)
+	{
+		leftMotor.setSpeed(0);
+		rightMotor.setSpeed(5);
+	}
+
+	else
+	{
+		leftMotor.setSpeed(5);
+		rightMotor.setSpeed(0);
+	}
+
 }
-*/
